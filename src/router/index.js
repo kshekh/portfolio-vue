9ca55c8d86b8f8b1575912438b4f1210c44d5807
Mohammmad Kamran Shekh <kshekh@gmail.com>
2022-12-08 23:09:03 +0530

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,51 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import Home from '@/views/HomeView.vue';
+import Customers from '@/views/CustomersView.vue';
+import CreateAccount from '@/components/CreateAccount.vue';
+import Login from '@/components/Login.vue';
+import ForgotPassword from '@/components/ForgotPassword.vue';
+
+const routes = [{
+        path: '',
+        name: '',
+        component: Login,
+        meta: { title: 'BrowTricks | Login' },
+    },
+    {
+        path: '/signup',
+        name: 'Create Account',
+        component: CreateAccount,
+        meta: { title: 'BrowTricks | Create Account' },
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: { title: 'BrowTricks | Login' },
+    },
+    {
+        path: '/forgot-password',
+        name: 'forgot-password',
+        component: ForgotPassword,
+        meta: { title: 'BrowTricks | Forgot Password' },
+    },
+    {
+        path: '/home',
+        name: 'Home',
+        component: Home,
+        meta: { title: 'BrowTricks | Home' }
+    },
+    {
+        path: '/customers',
+        name: 'Customers',
+        component: Customers,
+        meta: { title: 'BrowTricks | Customers' }
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <HomeCustomer />
+  </div>
+</template>
+
+<script setup>
+import HomeCustomer from '@/components/HomeCustomer.vue';
+</script>
